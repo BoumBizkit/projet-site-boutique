fb3f4c1c32fc3e3367fda1c26678e0832b9c6b26
yann <yannserranobouquier@gmail.com>
2026-02-05 11:50:06 +0100

    Update NSIProjet3.js

--- scripts/NSIProjet3.js
+++ scripts/NSIProjet3.js
@@ -20,6 +20,9 @@ var init = function () {
 	
 	// TODO : add other initializations to achieve if you think it is required
 }
+var filter = function() {
+	alert ("Coucou");
+}
 window.addEventListener("load", init);
 
 
